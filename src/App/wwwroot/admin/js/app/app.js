2a1ee13f72bda8617b099625a4e312679164b29f
Robin <robin.jiang@siloon.com>
2019-12-24 14:53:24 +0800

    BFM-473  add post un-saved changes prompt message

--- src/App/wwwroot/admin/js/app/app.js
+++ src/App/wwwroot/admin/js/app/app.js
@@ -148,3 +148,15 @@ function fail(jqXHR, exception) {
 }
 
 function emptyCallback() { }
+
+
+function difference(object, base) {
+    function changes(object, base) {
+        return _.transform(object, function (result, value, key) {
+            if (!_.isEqual(value, base[key])) {
+                result[key] = (_.isObject(value) && _.isObject(base[key])) ? changes(value, base[key]) : value;
+            }
+        });
+    }
+    return changes(object, base);
+}
